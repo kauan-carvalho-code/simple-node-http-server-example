7b0dccb9d8621199aa249dbc0406a12de7d92465
Kauan Carvalho <kauancarvalho987@gmail.com>
2025-02-08 23:55:19 -0300

    feat: implement JSON-based persistence for Database class

--- src/database.js
+++ src/database.js
@@ -1,7 +1,24 @@
+import fs from 'node:fs';
+
 import crypto from 'node:crypto';
+
+const databasePath = new URL('../database.json', import.meta.url);
 
 export class Database {
   #database = new Map();
+
+  constructor() {
+    if (fs.existsSync(databasePath)) {
+      const data = fs.readFileSync(databasePath, 'utf8');
+      this.#database = new Map(Object.entries(JSON.parse(data)));
+    } else {
+      this.#persist();
+    }
+  }
+
+  #persist() {
+    fs.writeFileSync(databasePath, JSON.stringify(Object.fromEntries(this.#database)));
+  }
 
   select(key) {
     if (!this.#database.has(key)) {
@@ -16,15 +33,15 @@ export class Database {
       this.#database.set(key, []);
     }
 
-    const id = data.id ?? crypto.randomBytes(16).toString('hex')
-
     const newData = {
-      id,
+      id: crypto.randomUUID(),
       ...data,
       createdAt: new Date().toISOString(),
     }
 
     this.#database.get(key).push(newData);
+
+    this.#persist()
 
     return newData
   }
